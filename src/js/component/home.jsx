c388db44d2d74feb64e1bf8d3a7c354d50e06f6f
Bforti <139017911+Bforti@users.noreply.github.com>
2023-12-23 02:10:12 +0000

    entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,22 +3,18 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
+
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex">
+			<h1>{props.cinco}</h1>
+			<h1>{props.cuatro}</h1>
+			<h1>{props.tres}</h1>
+			<h1>{props.dos}</h1>
+			<h1>{props.uno}</h1>
+			
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,17 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let counter=0
+setInterval(()=>{
+    let uno=    Math.floor((counter)%10);
+    let dos=    Math.floor((counter/10)%10);
+    let tres=   Math.floor((counter/100)%10);
+    let cuatro= Math.floor((counter/1000)%10);
+    let cinco=  Math.floor((counter/1000)%10);
+    counter ++;
+    ReactDOM.render(<Home uno={uno} dos={dos} tres={tres} cuatro={cuatro} cinco={cinco} />, document.querySelector("#app"));
+
+
+},1000)
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
